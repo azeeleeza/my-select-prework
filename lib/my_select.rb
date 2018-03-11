c468843015e49757e21424bbb3b087ec747717a7
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-11 00:33:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
   i=0
   new_collection=[]
   while i < collection.length
-    new_collection << yield(collection[i])
+    if yield(collection[i])
+      new_collection<<collection[i]
+    end
     i+=1
   end
   new_collection
